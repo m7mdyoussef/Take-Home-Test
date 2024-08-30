0311aaa1c778623148266d34a9aa37acc0cba218
mohamed youssef <mohamed.youssef@mdi.com.eg>
2024-08-30 16:18:50 +0300

    build App folders and Arch

---
+++ Take-Home/Take-Home/ApplicationSupport/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Take-Home
+//
+//  Created by JOE on 30/08/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
