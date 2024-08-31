4d23342daa5693e5cf09311f9d8e3731f42705c6
mohamed youssef <mohamed.youssef@mdi.com.eg>
2024-08-31 16:58:17 +0300

    add AsyncImageView custom view for image size pixel control

--- Take-Home/Take-Home/Utils/Extensions/UIImage+Extension.swift
+++ Take-Home/Take-Home/Utils/Extensions/UIImage+Extension.swift
@@ -18,6 +18,15 @@ extension UIImage {
     }
 }
 
+extension UIImage {
+    func resized(to targetSize: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+    }
+}
+
 extension Image {
     enum Character {
         static let imagePlaceholder = "imagePlaceHolder".image
